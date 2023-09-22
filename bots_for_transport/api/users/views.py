b986043670be146f939395f3201003e3c80cae55
Yuliya Kryuchkova <kissanni@mail.ru>
2023-09-22 20:30:37 +0700

    Убрать пагинотор из users

--- bots_for_transport/api/users/views.py
+++ bots_for_transport/api/users/views.py
@@ -1,10 +1,9 @@
 from django.contrib.auth import get_user_model
 from djoser.views import UserViewSet
 
-from api.pagination import LimitPageNumberPagination
 
 User = get_user_model()
 
 
 class CustomDjoserUserViewSet(UserViewSet):
-    pagination_class = LimitPageNumberPagination
+    pagination_class = None
