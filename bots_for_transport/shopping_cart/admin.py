ce8750d7920ff5dcb4189bb0970c87a367998bc9
Екатерина Тарасенко <ekaterina.tarasenko1@yandex.ru>
2023-09-22 11:27:28 +0300

    shopping_cart

--- bots_for_transport/shopping_cart/admin.py
+++ bots_for_transport/shopping_cart/admin.py
@@ -5,6 +5,6 @@ from .models import Shopping_cart
 
 @admin.register(Shopping_cart)
 class Shopping_cartAdmin(admin.ModelAdmin):
-    list_display = ('name', 'user')
-    search_fields = ('name', 'user')
+    list_display = ('bot', 'user')
+    search_fields = ('bot', 'user')
     empty_value_display = '-пусто-'
