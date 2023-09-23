eea2470d464b4b2d7ec3fdfa1137d2198d2caf44
Байбанов Эльдар <baibanove@mail.ru>
2023-09-23 04:54:12 +0300

    Добавил вьюху, урлы и сериализатор в API

---
+++ bots_for_transport/api/favorite/views.py
@@ -0,0 +1,40 @@
+from bot.models import Bot
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from favorite.models import Favorite
+from favorite.serializers import FavoriteSerializer
+
+
+class FavoriteView(APIView):
+
+    permission_classes = [IsAuthenticated, ]
+
+    def post(self, request, id):
+        data = {
+            'user': request.user.id,
+            'bot': id
+        }
+        bot = get_object_or_404(Bot, id=id)
+        if not Favorite.objects.filter(
+           user=request.user, bot=bot).exists():
+            serializer = FavoriteSerializer(
+                data=data, context={'request': request}
+            )
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        bot = get_object_or_404(Bot, id=id)
+        if Favorite.objects.filter(
+           user=request.user, bot=bot).exists():
+            Favorite.objects.filter(
+                user=request.user, bot=bot
+            ).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
