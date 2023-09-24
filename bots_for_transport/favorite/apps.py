dc3905f7c2dea915600875b71d4ff28f13eeab73
Yuliya Kryuchkova <kissanni@mail.ru>
2023-09-24 16:09:44 +0700

    Русифицировать названия приложений, в пред.пуше исправить в urls.py(bots на bot)

--- bots_for_transport/favorite/apps.py
+++ bots_for_transport/favorite/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class FavoriteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'favorite'
+    verbose_name = 'Избранный'
+    verbose_name_plural = 'Избранные'
--- bots_for_transport/shopping_cart/apps.py
+++ bots_for_transport/shopping_cart/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class Shopping_cartConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shopping_cart'
+    verbose_name = 'Корзина'
+    verbose_name_plural = 'Корзины'
