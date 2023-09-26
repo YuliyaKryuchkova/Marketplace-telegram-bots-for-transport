11226e9191b21f4fe440a73ebc0ebb89eb7b0e56
Yuliya Kryuchkova <kissanni@mail.ru>
2023-09-26 13:49:53 +0700

    Исправить восстановление пароля в settings

--- bots_for_transport/bots_for_transport/settings.py
+++ bots_for_transport/bots_for_transport/settings.py
@@ -105,12 +105,15 @@ DJOSER = {
     'PERMISSIONS': {
         'user': ['djoser.permissions.CurrentUserOrAdminOrReadOnly'],
     },
+    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': True,
     'LOGIN_FIELD': 'email',
     'SERIALIZERS': {
         'user_create': 'api.users.serializers.CustomUserCreateSerializer',
         'user': 'api.users.serializers.CustomUserSerializer',
         'current_user': 'api.users.serializers.CustomUserSerializer',
-        'password_reset': 'djoser.serializers.SendEmailResetSerializer',
     },
 
 }
