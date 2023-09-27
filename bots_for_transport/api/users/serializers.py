975371f731a6b71547aa060e7d196546dd737ef4
Yuliya Kryuchkova <kissanni@mail.ru>
2023-09-27 12:04:32 +0700

    Исправить регистрацию пользователя, добавить валидацию на совпадение пароля

--- bots_for_transport/api/users/serializers.py
+++ bots_for_transport/api/users/serializers.py
@@ -1,22 +1,35 @@
 from django.contrib.auth import get_user_model
-from djoser.serializers import UserCreateSerializer, UserSerializer, \
-    SendEmailResetSerializer
+from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 
 User = get_user_model()
 
 
 class CustomUserCreateSerializer(UserCreateSerializer):
+    confirm_password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = (
             'id',
             'email',
             'username',
-            'first_name',
-            'last_name',
-            'password'
+            'password',
+            'confirm_password'
         )
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data['email']
+        )
+        return user
 
     def validate_username(self, value):
         if value.lower() == 'me':
@@ -25,7 +38,6 @@ class CustomUserCreateSerializer(UserCreateSerializer):
 
 
 class CustomUserSerializer(UserSerializer):
-
     class Meta:
         model = User
         fields = (
