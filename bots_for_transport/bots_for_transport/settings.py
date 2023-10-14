262f320e15409c28af0dbd0caaf1ffdd01f7224e
Yuliya Kryuchkova <kissanni@mail.ru>
2023-10-14 23:11:46 +0700

    Добавить склайт3

--- bots_for_transport/bots_for_transport/settings.py
+++ bots_for_transport/bots_for_transport/settings.py
@@ -72,12 +72,14 @@ AUTH_USER_MODEL = 'users.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': env('DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': env('POSTGRES_DB', 'db.sqlite3'),
-        'USER': env('POSTGRES_USER', 'django'),
-        'PASSWORD': env('POSTGRES_PASSWORD', ''),
-        'HOST': env('DB_HOST', ''),
-        'PORT': env('DB_PORT', 5432)
+        # 'ENGINE': env('DB_ENGINE', 'django.db.backends.sqlite3'),
+        # 'NAME': env('POSTGRES_DB', 'db.sqlite3'),
+        # 'USER': env('POSTGRES_USER', 'django'),
+        # 'PASSWORD': env('POSTGRES_PASSWORD', ''),
+        # 'HOST': env('DB_HOST', ''),
+        # 'PORT': env('DB_PORT', 5432)
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
