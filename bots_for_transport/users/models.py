fddbc07625743de910466cd996d0a76c14a377f2
BankaiSS <baibanove@mail.ru>
2023-10-26 10:21:10 +0300

    Добавил возможность авторизации с помощью логина и почты, а также валидацию почты

--- bots_for_transport/users/models.py
+++ bots_for_transport/users/models.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.core.exceptions import ValidationError
 from django.db import models
-
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
 class User(models.Model):
     """Модель пользователя"""
@@ -33,6 +36,24 @@ class User(models.Model):
         blank=True
     )
 
+
+    def login(request):
+        if request.method == 'POST':
+            form = AuthenticationForm(request.POST)
+            if form.is_valid():
+                email = form.cleaned_data['email']
+                password = form.cleaned_data['password']
+                user = authenticate(request, email=email, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect('posts:index')
+                else:
+                    form.add_error(None, 'Неправильные почта или пароль')
+        else:
+            form = AuthenticationForm()
+        return render(request, 'users/login.html', {'form': form})
+    
+
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
@@ -44,3 +65,5 @@ class User(models.Model):
         super().clean()
         if self.username == 'me':
             raise ValidationError('Имя пользователя не может быть "me"')
+        if User.objects.filter(email=self.email).exists():
+            raise ValidationError('Пользователь с такой почтой уже существует')
